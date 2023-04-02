f7a49188d97e83304ca73e9f93a3ff5807b7d91a
kelec <kelec@LAPTOP-JGFOV90A>
2023-04-02 17:10:00 -0400

    Add project files.

---
+++ Program 2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guests = new System.Windows.Forms.Label();
+            this.nights = new System.Windows.Forms.Label();
+            this.stars = new System.Windows.Forms.Label();
+            this.comboBoxName = new System.Windows.Forms.ComboBox();
+            this.nightsTxt = new System.Windows.Forms.TextBox();
+            this.guestsTxt = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.cost = new System.Windows.Forms.Label();
+            this.costLbL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guests
+            // 
+            this.guests.AutoSize = true;
+            this.guests.Location = new System.Drawing.Point(124, 66);
+            this.guests.Name = "guests";
+            this.guests.Size = new System.Drawing.Size(95, 13);
+            this.guests.TabIndex = 0;
+            this.guests.Text = "Number of Guests:";
+            // 
+            // nights
+            // 
+            this.nights.AutoSize = true;
+            this.nights.Location = new System.Drawing.Point(127, 91);
+            this.nights.Name = "nights";
+            this.nights.Size = new System.Drawing.Size(92, 13);
+            this.nights.TabIndex = 1;
+            this.nights.Text = "Number of Nights:";
+            // 
+            // stars
+            // 
+            this.stars.AutoSize = true;
+            this.stars.Location = new System.Drawing.Point(157, 118);
+            this.stars.Name = "stars";
+            this.stars.Size = new System.Drawing.Size(62, 13);
+            this.stars.TabIndex = 2;
+            this.stars.Text = "Hotel Stars:";
+            // 
+            // comboBoxName
+            // 
+            this.comboBoxName.FormattingEnabled = true;
+            this.comboBoxName.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBoxName.Location = new System.Drawing.Point(225, 115);
+            this.comboBoxName.Name = "comboBoxName";
+            this.comboBoxName.Size = new System.Drawing.Size(83, 21);
+            this.comboBoxName.TabIndex = 3;
+            // 
+            // nightsTxt
+            // 
+            this.nightsTxt.Location = new System.Drawing.Point(225, 88);
+            this.nightsTxt.Name = "nightsTxt";
+            this.nightsTxt.Size = new System.Drawing.Size(83, 20);
+            this.nightsTxt.TabIndex = 4;
+            // 
+            // guestsTxt
+            // 
+            this.guestsTxt.Location = new System.Drawing.Point(225, 62);
+            this.guestsTxt.Name = "guestsTxt";
+            this.guestsTxt.Size = new System.Drawing.Size(83, 20);
+            this.guestsTxt.TabIndex = 5;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(192, 157);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 6;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // cost
+            // 
+            this.cost.AutoSize = true;
+            this.cost.Location = new System.Drawing.Point(160, 211);
+            this.cost.Name = "cost";
+            this.cost.Size = new System.Drawing.Size(59, 13);
+            this.cost.TabIndex = 7;
+            this.cost.Text = "Hotel Cost:";
+            // 
+            // costLbL
+            // 
+            this.costLbL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costLbL.Location = new System.Drawing.Point(225, 210);
+            this.costLbL.Name = "costLbL";
+            this.costLbL.Size = new System.Drawing.Size(83, 20);
+            this.costLbL.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 302);
+            this.Controls.Add(this.costLbL);
+            this.Controls.Add(this.cost);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.guestsTxt);
+            this.Controls.Add(this.nightsTxt);
+            this.Controls.Add(this.comboBoxName);
+            this.Controls.Add(this.stars);
+            this.Controls.Add(this.nights);
+            this.Controls.Add(this.guests);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guests;
+        private System.Windows.Forms.Label nights;
+        private System.Windows.Forms.Label stars;
+        private System.Windows.Forms.ComboBox comboBoxName;
+        private System.Windows.Forms.TextBox nightsTxt;
+        private System.Windows.Forms.TextBox guestsTxt;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label cost;
+        private System.Windows.Forms.Label costLbL;
+    }
+}
+
---
+++ Program 2/Form1.cs
@@ -0,0 +1,118 @@
+﻿//Grading ID: R2226
+// Program 2
+// 03/ 10/ 2022
+// CIS 199 - 02
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            //These are all the variables that will be used in the program
+            int guests;
+            int nights = 0;
+            int costperNight = 0;
+            int guestsNum;
+            double nightSum;
+            double totalcost;
+            double starcostfactor;
+
+            if (int.TryParse(guestsTxt.Text, out guests) && guests > 0 && guests < 7)// The if statement for the number of guests
+            {
+                if (int.TryParse(nightsTxt.Text, out nights) && nights > 0)// The int Try parse if statement for the number of nights
+                {
+                    if (comboBoxName.SelectedIndex > 0)// the if statement for the combobox selected name
+
+                    {
+                        //These are the if situations for what will be added for the selected Index based on the selected value from the combo box
+                        if (comboBoxName.SelectedIndex == 1)
+                        {
+                            starcostfactor = 1;
+                        }
+                        else if (comboBoxName.SelectedIndex == 2)
+                        {
+                            starcostfactor = 1.5;
+                        }
+                        else if (comboBoxName.SelectedIndex == 3)
+                        {
+                            starcostfactor = 2.5;
+                        }
+                        else if (comboBoxName.SelectedIndex == 4)
+                        {
+                            starcostfactor = 3;
+                        }
+                        else 
+                        {
+                            starcostfactor = 4;
+                        }
+                        // used the if-else  statements for the guests and the number that the amount of guests would equal for as well
+                        if (guests == 1)
+                        {
+                            guestsNum = 100;
+                        }
+                        else if (guests == 2)
+                        {
+                            guestsNum = 150;
+                        }
+                        else if (guests == 3)
+                        {
+                            guestsNum = 250;
+                        }
+                        else
+                        {
+                            guestsNum = 400;
+                        }
+                        // Used these if-else statements to describe the cost of the nights for how many nights that guest would be staying at the hotel
+                        if (nights >= 1 && nights <= 6)
+                        {
+                            costperNight = 100;
+                        }
+                        else if (nights >= 7 && nights <= 30)
+                        {
+                            costperNight =  75;
+                        }
+                        else 
+                        {
+                            costperNight = 25;
+                        }
+
+                        nightSum = nights * costperNight;// Used to calculate the amount of nights stayed by the cost per night
+                        totalcost = (nightSum + guestsNum) * starcostfactor;// Used to calculate the complete total cost using the variables above
+                        costLbL.Text = $"{totalcost:C2}";// This is to show what the output label will show based of what the total cost was
+                    }
+                    else
+
+                    {
+                        Console.WriteLine("Invalid Input"); // these last else statements are for what will be shown if any of the inputs do not equal what the perimeter was given.
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+} 
